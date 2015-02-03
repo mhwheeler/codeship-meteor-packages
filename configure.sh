34f32828ee7fadf3c0247d527bbaf1dc762b9055
Max Nowack <max.nowack@oafmedium.de>
2015-02-03 10:08:34 +0100

    added final cleanup

--- configure.sh
+++ configure.sh
@@ -4,4 +4,8 @@
 wget https://raw.github.com/oafmedium/codeship-meteor-packages/master/start_test.js
 wget https://raw.github.com/oafmedium/codeship-meteor-packages/master/phantom_runner.js
 
+# run tests
 node start_test.js
+
+# cleanup
+rm start_test.js phantom_runner.js
