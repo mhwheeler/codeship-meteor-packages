386479c9220d6d60164d9317b0997b6d00099528
mhwheeler <mhwheeler@gmail.com>
2015-05-11 17:06:13 -0700

    node start_test.js should be done manually in codeship test settings

--- configure.sh
+++ configure.sh
@@ -4,8 +4,8 @@
 wget https://raw.github.com/mhwheeler/codeship-meteor-packages/master/start_test.js
 wget https://raw.github.com/mhwheeler/codeship-meteor-packages/master/phantom_runner.js
 
-# run tests
-node start_test.js
+# run tests (DO THIS IN YOUR CODESHIP TEST SETTINGS)
+# node start_test.js
 
-# cleanup
-rm start_test.js phantom_runner.js
+# cleanup (DO THIS IN YOUR CODESHIP TEST SETTINGS)
+# rm start_test.js phantom_runner.js
